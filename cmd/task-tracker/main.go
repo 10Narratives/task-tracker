68879ff54a13544399b7ce35b4f3246b3b12d054
10Narratives <vanyatertyshnikov@yandex.ru>
2025-01-30 13:54:18 +0300

    Add start up of HTTP server

--- cmd/task-tracker/main.go
+++ cmd/task-tracker/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"log/slog"
+	"net/http"
 
 	"github.com/10Narratives/task-tracker/internal/config"
-	"github.com/10Narratives/task-tracker/internal/logger"
+	"github.com/10Narratives/task-tracker/internal/http-server/mw_logger"
+	"github.com/10Narratives/task-tracker/internal/logging"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/joho/godotenv"
 )
 
@@ -16,11 +21,28 @@ func main() {
 
 	cfg := config.MustLoad()
 
-	logger := logger.MustLogger(cfg.Env)
-	logger.Info("Start up task-tracker", slog.String("env", cfg.Env))
+	logger := logging.MustLogger(cfg.Env)
+	logger.Info("Start up task-tracker",
+		slog.String("env", cfg.Env),
+		slog.String("address", cfg.HTTP.Address),
+		slog.String("port", cfg.HTTP.Port))
 	logger.Debug("Debug messages are enabled")
 
 	// TODO: Initialize storage
 
-	// TODO: Initialize http server
+	webDir := "./web"
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(mw_logger.New(logger))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+	router.Handle("/*", http.StripPrefix("/", http.FileServer(http.Dir(webDir))))
+
+	fullAddr := cfg.HTTP.Address + ":" + cfg.HTTP.Port
+	if err := http.ListenAndServe(fullAddr, router); err != nil {
+		fmt.Println(err)
+		logger.Error("Can not start up http server")
+	}
+
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.23.5
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.2.0 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
