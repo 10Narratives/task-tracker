9ac14a5bcf29780cf315d5d38bd522b4c10fb14a
10Narratives <vanyatertyshnikov@yandex.ru>
2025-02-09 10:34:31 +0300

    Add daily move in nextdate

--- internal/services/nextdate/nextdate.go
+++ internal/services/nextdate/nextdate.go
@@ -8,17 +8,13 @@ import (
 const DateLayout = "20060102"
 
 func shiftDaily(now, date time.Time, days int) time.Time {
-	for now.After(date) {
-		date = date.AddDate(0, 0, days)
-	}
-	return date
-}
+	diffDays := int(now.Sub(date).Hours() / 24)
 
-func shiftYearly(now, date time.Time) time.Time {
-	for now.After(date) {
-		date = date.AddDate(1, 0, 0)
+	if diffDays <= 0 {
+		return date.AddDate(0, 0, days)
 	}
-	return date
+
+	return date.AddDate(0, 0, diffDays+(days-diffDays%days))
 }
 
 // NextDate calculates the next occurrence of a date based on a given repetition pattern.
@@ -28,8 +24,6 @@ func NextDate(now, date time.Time, repeat string) string {
 	case 'd':
 		days, _ := strconv.Atoi(repeat[2:])
 		nextdate = shiftDaily(now, date, days).Format(DateLayout)
-	case 'y':
-		nextdate = shiftYearly(now, date).Format(DateLayout)
 	}
 	return nextdate
 }
--- internal/services/nextdate/nextdate_test.go
+++ internal/services/nextdate/nextdate_test.go
@@ -8,6 +8,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+/*
+	{"20240113", "d 7", `20240127`},
+	{"20240120", "d 20", `20240209`},
+	{"20240202", "d 30", `20240303`},
+	{"20240320", "d 401", ""},
+	{"20231225", "d 12", `20240130`},
+	{"20240228", "d 1", "20240229"},
+*/
+
+// 20240126
+
 func TestNextDate(t *testing.T) {
 	type args struct {
 		now    time.Time
@@ -21,13 +32,34 @@ func TestNextDate(t *testing.T) {
 		want string
 	}{
 		{
-			name: "successful daily move",
-			args: args{
-				now:    time.Date(2025, 2, 5, 0, 0, 0, 0, time.UTC),
-				date:   time.Date(2025, 2, 4, 0, 0, 0, 0, time.UTC),
-				repeat: "d 3",
-			},
+			name: "successful daily move - d 3",
+			args: args{now: time.Date(2025, 2, 5, 0, 0, 0, 0, time.UTC), date: time.Date(2025, 2, 4, 0, 0, 0, 0, time.UTC), repeat: "d 3"},
 			want: "20250207",
+		},
+		{
+			name: "successful daily move - d 7",
+			args: args{now: time.Date(2025, 2, 5, 0, 0, 0, 0, time.UTC), date: time.Date(2025, 2, 4, 0, 0, 0, 0, time.UTC), repeat: "d 7"},
+			want: "20250211",
+		},
+		{
+			name: "successful daily move - d 20",
+			args: args{now: time.Date(2024, 1, 26, 0, 0, 0, 0, time.UTC), date: time.Date(2024, 1, 20, 0, 0, 0, 0, time.UTC), repeat: "d 20"},
+			want: "20240209",
+		},
+		{
+			name: "successful daily move - d 30",
+			args: args{now: time.Date(2024, 1, 26, 0, 0, 0, 0, time.UTC), date: time.Date(2024, 2, 2, 0, 0, 0, 0, time.UTC), repeat: "d 30"},
+			want: "20240303",
+		},
+		{
+			name: "successful daily move - same day",
+			args: args{now: time.Date(2024, 1, 26, 0, 0, 0, 0, time.UTC), date: time.Date(2024, 1, 26, 0, 0, 0, 0, time.UTC), repeat: "d 1"},
+			want: "20240127",
+		},
+		{
+			name: "successful daily move - future",
+			args: args{now: time.Date(2024, 1, 26, 0, 0, 0, 0, time.UTC), date: time.Date(2024, 1, 27, 0, 0, 0, 0, time.UTC), repeat: "d 1"},
+			want: "20240128",
 		},
 	}
 
@@ -37,7 +69,7 @@ func TestNextDate(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			res := nextdate.NextDate(tc.args.now, tc.args.now, tc.args.repeat)
+			res := nextdate.NextDate(tc.args.now, tc.args.date, tc.args.repeat)
 			assert.Equal(t, tc.want, res)
 		})
 	}
